93994811905502d001fd97481dead52300e29e82
Orel Mizrahi Adani <76569702+orelmizrahii@users.noreply.github.com>
2022-11-22 04:34:55 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+Do NOT touch the index.php and style.css files.
+Everything you edit there has the Potential to break the Template.
+If you do it you will NOT get Support in fixing it.
+
---
+++ config.php
@@ -0,0 +1,153 @@
+<?php
+//INFO
+//To Change the Icon of the Website, replace the favicon.ico with your logo (Has to be .ico, and keep the name as favicon.ico)
+
+//Name of your Website
+$websitename = "Kudos Services";
+
+//This is what is Displayed as Tab name in the Browser, and as name of the search result on Google
+$websitetitle = "Kudos Services";
+
+//This is the Description displayed on Google
+$websitedescription = "Kudos Services , Providing customer services since July 2021";
+
+//These are Tags for Search Engines. They are Seperated by a comma
+$websitetags = "discord, accounts, cod tools, reseller";
+
+
+//Discord Link for the Button on the Bottom Right
+$discordlink = "https://discord.com/invite/T8ZHWhQn6E";
+
+//Purchase Button Text
+$purchasetext = "Purchase";
+
+//Name of the Ecommerce Website ("sellix" = Sellix.io, "shoppy" = Shoppy.gg, "selly" = Selly.io)
+$shopwebsite = "sellix";
+
+//These are the 3 Images displayed on the Top
+//There have to be exactly 3 Images
+$sliderimages = [
+    "images/slider1.jpg",
+    "images/slider2.jpg",
+    "images/slider3.jpg"
+];
+
+//Products
+$products = [
+    [
+        //Name of the Category
+        "Discord",
+        [
+            [
+                //Name of the Product
+                "Discord Server Boost",
+                //Product ID on the Ecommerce Website
+                "6243ade762966",
+                //Product Logo URL or Path (Should be Transparent or it Will be Displayed with the Background)
+                "https://media.sketchfab.com/models/7218d808cf2d46db9fcca7f96aebd76f/thumbnails/40a5430b011647bbafa078d38b0c919e/5fc1f8cf89ea4cfa8bbc77eb1ba2ce31.jpeg",
+                //Price of the Product
+                "$ 1.50",
+                //Description of the Product
+                "Discord server boosts, gain server perks by boosting your server to new perk levels!
+
+                Boosts will last for 3 months!"
+            ],
+            [
+                //Name of the Product
+                "Discord Token With 2 Boosts",
+                //Product ID on the Ecommerce Website
+                "622d01bb6c234",
+                //Product Logo URL or Path (Should be Transparent or it Will be Displayed with the Background)
+                "https://c.tenor.com/-iJ1olfz7qsAAAAC/boost-discord.gif",
+                //Price of the Product
+                "$ 1.75",
+                //Description of the Product
+                "These are Discord tokens with 3 months of nitro, 2 boosts per token"
+            ],
+            [
+                //Name of the Product
+                "3 Months Nitro Promo Links",
+                //Product ID on the Ecommerce Website
+                "62524abb882cd",
+                //Product Logo URL or Path (Should be Transparent or it Will be Displayed with the Background)
+                "https://i.ytimg.com/vi/PxH2vM-YS5k/maxresdefault.jpg",
+                //Price of the Product
+                "$ 2.50",
+                //Description of the Product
+                "3 Months of Nitro
+                2 Server Boosts  
+                ONLY FOR NEW ACCOUNTS OR ACCOUNTS THAT HAVE NEVER HAD NITRO!"
+            ],
+            [
+                //Name of the Product
+                "Boost Bot 2.0 - Lifetime",
+                //Product ID on the Ecommerce Website
+                "6252728c90661",
+                //Product Logo URL or Path (Should be Transparent or it Will be Displayed with the Background)
+                "https://www.apkmirror.com/wp-content/uploads/2021/12/91/61b7a0ecb8d05.png",
+                //Price of the Product
+                "$ 50.00",
+                //Description of the Product
+                "Boost Bot
+                Information
+                Boost Bot is the most useful and efficient discord boost bot, allowing you to add your own stock and boost servers without having to login to your accounts."
+            ],
+            [
+                "Boost Bot 2.0 - Source Code",
+                "6252734031f3b",
+                "https://play-lh.googleusercontent.com/9_XL4vQnZf9HrwqCxO32sJHPUYqFOmhip8HyRPo9g8jrdoGKp3uOEMPFFPVFHcZOKTw",
+                "$ 250.00",
+                "Boost Bot
+                Information
+                Boost Bot is the most useful and efficient discord boost bot, allowing you to add your own stock and boost servers without having to login to your accounts."
+            ]
+        ]
+    ],
+    [
+        //Name of the Category
+        "Services",
+        [
+            [
+                //Name of the Product
+                "Aged Acti's (Bnet Linkable)",
+                //Product ID on the Ecommerce Website
+                "6254e26681a17",
+                //Product Logo URL or Path (Should be Transparent or it Will be Displayed with the Background)
+                "images/slider2.jpg",
+                //Price of the Product
+                "$ 1.95",
+                "Aged Activision Accounts - Battle.net Linkable
+
+                Each account will be an unchecked Call of Duty (Activision) account
+                I am not selling stats only, these are un-botted aged accounts and may contain rare camos/packs inside.
+                all unlinked from battle.net so you can play on PC"
+            ],
+            [
+                "DA Tool (24 hour)",
+                "62432dc34bd38",
+                "images/slider1.jpg",
+                "$ 25.00",
+                "100% Undetected. No accounts (hosts or clients) have been banned from using this tool"
+            ],
+            [
+                "Aged Acti's (Bnet Linkable) (Reseller)",
+                "6255012167669",
+                "images/slider3.jpg",
+                "$ 10.00",
+                "Aged Activision Accounts - Battle.net Linkable"
+            ]
+        ]
+    ]
+];
+
+//Every Color you Can customize is in Here
+$colorconfig = [
+    "#0A1730", //Header Background Color
+    "#6a71ff", //Purchase Button Color
+    "#3b35e7", //Underline of Category Titles
+    "#28a745", //Price Background Color
+    "#1f2cb8", //Product Title Color
+    "#7289d9" //Discord Icon Background Color
+];
+
+?>
